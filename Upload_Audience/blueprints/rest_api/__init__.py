f6550af354c7c599a03e57f157c5a10eeb30cecf
DevEduardoOliveira <duduoliveiram54@gmail.com>
2024-06-14 13:17:25 -0300

    Ajust

--- Upload_Audience/blueprints/rest_api/__init__.py
+++ Upload_Audience/blueprints/rest_api/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask_restful import Api
-from .resources import AudienceItemResource, AudienceResource, DeleteAudience, ProcessSuccessAudience
+from .resources import AudienceItemResource, AudienceResource, DeleteAudience, ProcessSuccessAudience, ListProcessedAudience
 
 """O endpoint(blueprint) '/api/v1/list_audiences' realiza a busca no banco de dados da aplicação [integration_last_15d] para disponibilizar o json que servirá de insumo para outros processos."""
 
@@ -10,7 +10,8 @@ api = Api(bp)
 api.add_resource(AudienceResource, '/list_audiences/')
 api.add_resource(AudienceItemResource, '/list_audiences/<parceiro>/<audience_id>')
 api.add_resource(DeleteAudience, '/delete/<parceiro>/<audience_id>')
-api.add_resource(ProcessSuccessAudience, '/list_processed_audiences/<parceiro>/<audience_id>')
+api.add_resource(ProcessSuccessAudience, '/process_audiences/<parceiro>/<audience_id>')
+api.add_resource(ListProcessedAudience, '/list_processed_audiences')
 
 def init_app(app):
     app.register_blueprint(bp)
--- Upload_Audience/blueprints/rest_api/resources.py
+++ Upload_Audience/blueprints/rest_api/resources.py
@@ -110,3 +110,46 @@ class ProcessSuccessAudience(Resource):
             database_operations.execute('process_success_audience', parceiro=parceiro, audience_id=audience_id)
         except:
             return abort(404)
+
+class ListProcessedAudience(Resource):
+    @auth.login_required
+    def get(self):
+        audiences = list_existing_audiences(processed_audiences = True)
+        audiences_sf = list_existing_audiences_salesforce(processed_audiences = True)
+        json_tk = {
+            "Created_Audiences": [
+                {
+                    "audience_id": audience[0],
+                    "db_name": audience[2],
+                    "table_name": audience[3],
+                    "audience_name": audience[4],
+                    "platform": audience[5],
+                    "advertiser_name": audience[6],
+                    "created_by": audience[7]
+                } for audience in audiences
+            ]
+        }
+
+        json_sf = {
+            "Created_Audiences": [
+                {
+                  "audience_id": audience[0],
+                  "db_name_sf": audience[2],
+                  "table_name_sf": audience[3],
+                  "file_name": audience[4],
+                  "platform": audience[5],
+                  "sftp_path": audience[6],
+                  "created_by": audience[7]
+                  } for audience in audiences_sf
+            ]
+        }
+        # Combine the audience lists
+        combined_audiences = json_tk["Created_Audiences"] + json_sf["Created_Audiences"]
+
+        # Create the final JSON structure
+        final_json = {"Created_Audiences": combined_audiences}
+
+        json_response = json.dumps(final_json, ensure_ascii=False, indent=2).encode('utf-8')
+
+        # Retornar a resposta com Content-Type definido como application/json
+        return Response(json_response, content_type='application/json')
